2efb772127b879565da7cb6b6b7a18ffdc289b7d
Elsabet <elsabetshiferaw1611199@gmail.com>
2024-12-16 13:31:16 +0300

    Update the UI

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/', views.post_list, name='post_list'),
+    path('posts/', views.post_list, name='blog'),
     path('posts/<slug:slug>/', views.post_detail, name='post_detail'),
 ]
